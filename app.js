4b4d50d0cc57b8625294e08b2c48f0b8cc9ede8a
Sohel Rana <mdsohelkhan418126@gmail.com>
2026-03-27 10:15:46 +0600

    {commit_message}

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let idbyname =""
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
